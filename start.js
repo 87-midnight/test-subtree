0575338bafa3c2e066463040ae1a1843b83d5bf8
AnnieCattice <AnnieCattice@hotmail.com>
2021-03-30 21:29:16 +0800

    test

--- start.js
+++ start.js
@@ -1,3 +1,13 @@
 const prints = (param) =>{
     console.log(param)
 }
+
+function query(){
+    let search_element = document.evaluate("//div[@class='search'/input")
+    let node = search_element.iterateNext()
+    while (node){
+        node.value="xxx";
+        node.click()
+        node = search_element.iterateNext()
+    }
+}
